b75b416236a20ccbc618381ea8e1191e12aed714
pro_c <hieutg244@gmail.com>
2025-04-28 00:33:46 +0700

    upppp

--- Assets/Scripts/SettingPopup.cs
+++ Assets/Scripts/SettingPopup.cs
@@ -4,33 +4,33 @@ using UnityEngine.UI;
 
 public class SettingPopup : MonoBehaviour
 {
-    [SerializeField] private GameObject popup;
+	[SerializeField] private GameObject popup;
 
-    [SerializeField] private Image musicOn, mucsicOff;
+	[SerializeField] private Image musicOn,mucsicOff;
 
-    [SerializeField] private Image soundOn, soundOff;
+	[SerializeField] private Image soundOn,soundOff;
 
-    [SerializeField] private Sprite onImage;
+	[SerializeField] private Sprite onImage;
 
-    public void MusicChange(bool value)
-    {
-        this.musicOn.sprite = value ? onImage : null;
-        this.mucsicOff.sprite = value ? null : onImage;
-    }
+	public void MusicChange(bool value)
+	{
+		this.musicOn.gameObject.SetActive(value);
+		this.mucsicOff.gameObject.SetActive(!value);
+	}
 
-    public void SoundChange(bool value)
-    {
-        this.soundOn.sprite = value ? onImage : null;
-        this.soundOff.sprite = value ? null : onImage;
-    }
+	public void SoundChange(bool value)
+	{
+		this.soundOn.gameObject.SetActive(value);
+		this.soundOff.gameObject.SetActive(!value);
+	}
 
-    public void Show(bool value)
-    {
-        if (value)
-        {
-            Singleton<SoundManager>.Instance.Play("Popup");
-        }
+	public void Show(bool value)
+	{
+		if(value)
+		{
+			Singleton<SoundManager>.Instance.Play("Popup");
+		}
 
-        this.popup.SetActive(value);
-    }
+		this.popup.SetActive(value);
+	}
 }
